6a00ae284bdc5af7f9bc9b5f3f5e53d52a0ae788
LEAN-arch <jbautistamsc@gmail.com>
2025-08-08 19:50:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import io
-import matplotlib as plt
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
